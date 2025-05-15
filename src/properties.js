caba8de82adaed796eab48f865c651b5b382b9ac
KAVIN-ARUN <kavin.arun@yahoo.co.uk>
2025-05-15 01:43:40 +0100

    Update properties.js

--- src/properties.js
+++ src/properties.js
@@ -56,7 +56,7 @@ var editable = true;
  * @type {{html: string, id: string}}
  */
 var paper = {
-    html: 'Sample literature collection for <b><a href="http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/eurovis14-star.pdf" target="_blank">SurVis</a></b>',
+    html: 'Sample literature collectionss for <b><a href="http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/eurovis14-star.pdf" target="_blank">SurVis</a></b>',
     id: 'Beck2016Visual'
 };
 
